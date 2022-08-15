b9fd007e9c5ec6bb42c8dc46b9d388d0710b24ec
Brett Abraham <brettabraham@Macbeth.local>
2022-08-15 11:14:35 +0100

    update: added details of how to run the app in README.md - removed unused files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # inthestyleapp
+
+# Prerequisites
+* xcode
+* homebrew/node
+* react-native
+This app was created using expo init
+
+# To start the app, please run the following in the terminal
+* npm install
+* expo start
+* (press i on the terminal to open iphone simulator)
+
+# To run the coding challenge (saved in file __tests__/walkChallenge.test.ts) please run the follwing command in the terminal
+* npm test
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,10 +1,9 @@
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { ColorSchemeName } from 'react-native';
 import { UserProvider } from '../context/user.context';
 import BrowseScreen from '../screens/browse/browseScreen';
-import ProfileScreen from '../screens/profile/profileScreen';
 import { CustomDarkTheme, CustomDefaultTheme } from '../style/themes';
 import LinkingConfiguration from './LinkingConfiguration';
 
@@ -24,9 +23,6 @@ function RootNavigator() {
     return (
         <Stack.Navigator>
             <Stack.Screen name="BrowseScreen" component={BrowseScreen} options={{ headerShown: false }} />
-            <Stack.Group screenOptions={{ presentation: 'modal' }}>
-                <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-            </Stack.Group>
         </Stack.Navigator>
     );
 }
